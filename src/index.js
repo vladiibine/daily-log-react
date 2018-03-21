7a78ac79ae701ed7677a03e79a662b2026aea847
Vlad Ardelean <vlad.ardelean@trustyou.net>
2018-03-21 20:31:13 +0200

    Added an editor, hooked up some event handlers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
+class MyEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    }
+
+    handleKeyCommand(command, editorState){
+        debugger;
+        console.log('vlad was here 2');
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+        if (newState){
+            this.onChange(newState);
+            return 'handled'
+        }
+        return 'not-handled'
+    }
+
+    render() {
+        return (
+            <Editor editorState={this.state.editorState} onChange={this.onChange} handleKeyCommand={this.handleKeyCommand}/>
+        );
+    }
+}
+
+ReactDOM.render(
+    <div>
+        asdf
+        <MyEditor/>
+        zxcv
+    </div>,
+    document.querySelector('.container')
+);
